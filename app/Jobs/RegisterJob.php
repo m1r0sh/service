1ac472239c3988888561ad1c572649fb496c63b0
@aitzhanov_meiirlikhan <mirosh753@gmail.com>
2023-07-13 23:44:47 +0500

    permission

---
+++ app/Jobs/RegisterJob.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
+class RegisterJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        protected $email,
+        protected $user,
+    ){}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to($this->email)->send(new \App\Mail\ServiceCreated($this->user));
+    }
+}
